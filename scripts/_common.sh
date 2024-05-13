6aff50e305798494132ec880dcaedc6bce59bf88
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-05-13 13:27:09 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # NodeJS version
-nodejs_version=14
+nodejs_version=18
 
 #=================================================
 # PERSONAL HELPERS
